ede900bfd1d989ae68e16ba4ce6fda8727cb8569
Alexandre Pereira <alexandre@placecode.com.br>
2022-04-23 16:01:03 -0300

    Configuring pages home and posts

---
+++ src/pages/components/ActiveLink/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { cloneElement } from 'react';
+import { useRouter } from 'next/router';
+import { LinkInterface } from '../../../interfaces/LinkInterface';
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...otherProps
+}: LinkInterface) {
+  const { asPath } = useRouter();
+  const className = asPath === otherProps.href ? activeClassName : '';
+
+  return <Link {...otherProps}>{cloneElement(children, { className })}</Link>;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1>
+      Olá <span>Dev!</span>
+    </h1>
+  );
+}
