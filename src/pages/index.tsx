9524f0427e6d9d31f27a4cbf33e0922d61459566
Alexandre Pereira <alexandre@placecode.com.br>
2022-04-24 11:27:02 -0300

    Configuring Server Side Rendering - SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import { GetServerSideProps } from 'next';
+import { PostInterface } from '../interfaces/PostInterface';
+interface HomeProps {
+  posts: PostInterface[];
+}
+
+export default function Home({ posts }: HomeProps) {
+  // Client Side Rendering (CSR)
+  // const [posts, setPosts] = useState<PostInterface[]>([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/posts').then(res => {
+  //     res.json().then(data => {
+  //       setPosts(data);
+  //     });
+  //   });
+  // }, []);
   return (
-    <h1>
-      Olá <span>Dev!</span>
-    </h1>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((item, index) => (
+          <li key={index}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
+//Server Side Rendering (SSR)
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
